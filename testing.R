c61a1bf04c4e1762cdacb698a7bcfd154a06fd86
Jhony Benavides <iajabenavi@gmail.com>
2019-12-25 11:55:32 -0500

    First change after first commit

--- testing.R
+++ testing.R
@@ -1,3 +1,8 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+print("This is a text I added after 
+      I did my first commit to check
+      how changes will be reported
+      in RStudio and in GitHub")
